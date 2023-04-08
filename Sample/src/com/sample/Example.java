ee0d843a0f76867e852c4ffd6e507fea048e9bff
prtribhuvan <pallavitribhuvan1997@gmail.com>
2023-04-08 23:42:43 +0530

    Division of two number

---
+++ Sample/src/com/sample/Example.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class Example {
+	
+	public static void main(String[] args) {
+	
+	int a= 20 ;
+	int b=10;
+	int c = a/b;
+	
+	System.out.println("Division of two number is:"+c);
+   
+	}
+}
